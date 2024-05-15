b9235e29e85c485cb20ec8ed01f1087ff95f6e03
JordanBandur <jordanbandur@hotmail.ca>
2024-05-15 09:14:00 -0700

    Refactor to receive props

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,20 +2,16 @@ import React from "react";
 
 import "../styles/PhotoListItem.scss";
 
+const PhotoListItem = (props) => {
+  return (
+    <section id={props.id}>
+      <img src={props.imageSource}/>
+      <img src={props.profile} />
+      <p>{props.username}</p>
+      <p>{props.location.city}, {props.location.country}</p>
+    </section>
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
-  /* Insert React */
+  );
 };
 
 export default PhotoListItem;
