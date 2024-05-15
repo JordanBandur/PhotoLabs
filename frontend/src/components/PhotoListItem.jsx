8f49ee93163d61d32b3742ad474ea5302646a4b1
JordanBandur <jordanbandur@hotmail.ca>
2024-05-15 09:24:49 -0700

    Modify HTML & add prop validation

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -4,13 +4,26 @@ import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = ({ photo }) => {
   return (
-    <section id={photo.id}>
+    <article id={photo.id}>
       <img src={photo.imageSource} />
       <img src={photo.profile} />
       <p>{photo.username}</p>
       <p>{photo.location.city}, {photo.location.country}</p>
-    </section>
+    </article>
   );
+};
+
+PhotoListItem.propTypes = {
+  photo: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    imageSource: PropTypes.string.isRequired,
+    profile: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired,
+    location: PropTypes.shape({
+      city: PropTypes.string.isRequired,
+      country: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
 };
 
 export default PhotoListItem;
