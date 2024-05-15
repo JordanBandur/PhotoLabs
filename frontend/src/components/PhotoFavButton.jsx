72945784fc776713087d636254ce37c45316b6c1
JordanBandur <jordanbandur@hotmail.ca>
2024-05-15 14:40:54 -0700

    Implement PhotoFavButton

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,13 +1,18 @@
 import React, { useCallback, useState } from 'react';
-
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton() {
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  const handleFavClick = () => {
+    setIsFavorite(!isFavorite);
+  };
+
   return (
-    <div className="photo-list__fav-icon">
+    <div className="photo-list__fav-icon" onClick={handleFavClick}>
       <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+        <FavIcon selected={isFavorite} />
       </div>
     </div>
   );
