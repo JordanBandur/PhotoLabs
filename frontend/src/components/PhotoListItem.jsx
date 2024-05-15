a52b25fe8891dbab0926d31175fd9f43733c88ad
JordanBandur <jordanbandur@hotmail.ca>
2024-05-15 14:41:08 -0700

    Refactor to include FavButton

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-
 import "../styles/PhotoListItem.scss";
+import PhotoFavButton from './PhotoFavButton';
 
 const PhotoListItem = ({ photo }) => {
   return (
     <article className="photo-list__item">
-      <img src={photo.imageSource} alt={`${photo.username}'s photo`} className="photo-list__image"  />
+      <header>
+      <PhotoFavButton />
+      <img src={photo.imageSource} alt={`${photo.username}'s photo`} className="photo-list__image" />
+      </header>
       <section className="photo-list__user-details">
         <img src={photo.profile} alt={`${photo.username}'s profile`} className="photo-list__user-profile" />
         <section className="photo-list__user-info">
