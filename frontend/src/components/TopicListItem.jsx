d9584d0511c4f2ae23d75d5d1748d3cda0c11510
JordanBandur <jordanbandur@hotmail.ca>
2024-05-15 21:17:38 -0700

    Implement logic

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -1,17 +1,10 @@
 import React from "react";
-
 import "../styles/TopicListItem.scss";
 
-const sampleDataForTopicListItem = {
-  id: "1",
-  slug: "topic-1",
-  label: "Nature",
-};
-
-const TopicListItem = () => {
+const TopicListItem = ({topic}) => {
   return (
     <div className="topic-list__item">
-      {/* Insert React */}
+      <span>{topic.title}</span>
     </div>
   );
 };
