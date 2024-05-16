0abe2e6207bcfdde36f526a62973f7b920f266fa
JordanBandur <jordanbandur@hotmail.ca>
2024-05-15 21:17:03 -0700

    Import + add TopicList

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import PhotoList from 'components/PhotoList';
 import './App.scss';
+import TopicList from 'components/TopicList';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
 
   return (
     <div className="App">
+      <TopicList />
       <PhotoList />
     </div>
   );
