24a8c401a4b7dce6d5a6c97838930929f608b447
JordanBandur <jordanbandur@hotmail.ca>
2024-05-15 21:36:37 -0700

    Clean up code

--- frontend/src/components/FavBadge.jsx
+++ frontend/src/components/FavBadge.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import FavIcon from './FavIcon';
-
 import '../styles/FavBadge.scss';
 
 const FavBadge = ({ isFavPhotoExist }) => {
   return (
     <div className='fav-badge'>
-      <FavIcon displayAlert={!!isFavPhotoExist}/>
+      <FavIcon displayAlert={!!isFavPhotoExist} />
     </div>
-  ) 
+  );
 };
 
 export default FavBadge;
